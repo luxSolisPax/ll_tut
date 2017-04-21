2cfe68510390b131b67e5c980eccc3dc81f7ec86
luxSolisPax <benjamin.kevin.fang@gmail.com>
2017-04-21 15:53:21 -0500

    Add files via upload
    homework 1

--- list.c
+++ list.c
@@ -5,7 +5,7 @@ list_node_t *seek_tail(list_node_t *node);
 
 list_t *ll_new() {
   list_t *ret = (list_t *) malloc(sizeof(list_t));
-  ret->head = (void *)0;
+  ret->head = 0;
   ret->size = 0;
   return ret; }
 
@@ -39,4 +39,83 @@ list_node_t *seek_tail(list_node_t *node) {
   } else {
     return seek_tail(node->next);
   }
+}
+
+size_t ll_size(list_t *list) {
+    return list->size;
+}
+
+/* get a value in the list by index */
+int ll_get(list_t *list, int index){    
+    list_node_t *get_node = list->head;
+    
+    if(index >= ll_size(list)){
+        //error handling?
+        printf(" [!! ll_get: Index %Iu is out of bounds !!] ", index);
+        return -1;
+    } else {
+        while (index > 0) {
+            get_node = get_node->next;
+            index--;
+        }
+        return get_node->value;
+    }
+}
+
+/* set a value in the list by index */
+void ll_set(list_t *list, int index, int value) {
+    list_node_t *set_node = list->head;
+
+    if (index >= ll_size(list)) {
+        //error handling?
+        printf(" [!! ll_get: Index %Iu is out of bounds !!] ", index);
+    } else {
+        while (index > 0) {
+            set_node = set_node->next;
+            index--;
+        }
+        set_node->value = value;
+    }
+}
+
+void ll_remove(list_t *list, int index){
+    list_node_t *remove_node = list->head;
+    list_node_t *previous_node = NULL;
+    
+    if(index >= ll_size(list)){
+        printf(" [!! ll_get: Index %Iu is out of bounds !!] ", index);
+    } else {
+        list->size--;
+        if(index == 0){
+            list->head = remove_node->next;
+        } else {
+            while (index > 0) {
+                previous_node = remove_node;
+                remove_node = remove_node->next;
+                index--;
+            }
+            previous_node->next = remove_node->next;            
+        }
+
+    }
+}
+
+void ll_insert(list_t *list, int index, int value) {
+    list_node_t *new_node = (list_node_t*) malloc(sizeof (list_node_t));
+    new_node->value = value;
+    new_node->next = NULL;
+    
+    list_node_t *insert_node = list->head;
+    if(index >= ll_size(list)){
+        printf(" [!! ll_get: Index %Iu is out of bounds !!] ", index);
+    } else {
+        while (index > 0) {
+            insert_node = insert_node->next;
+            index--;
+        }
+        new_node->next = insert_node->next;
+        insert_node->next = new_node;
+        list->size++;
+    }
+
 }
--- ll_tut.c
+++ ll_tut.c
@@ -15,13 +15,57 @@ void print_list(list_t *list) {
 }
 
 int main(int argc, char *argv[]) {
-  list_t *list = ll_new();
+list_t *list = ll_new();
   ll_add(list, 1);
   ll_add(list, 2);
   ll_add(list, 3);
   ll_add(list, 4);
   ll_add(list, 5);
   print_list(list);
+  
+  //ll_get test
+  printf("index (0): ");
+  printf("%Iu", ll_get(list, 0));
+  printf("\n");
+  
+  printf("index (1): ");
+  printf("%Iu", ll_get(list, 1));
+  printf("\n");
+  
+  printf("index (2): ");
+  printf("%Iu", ll_get(list, 2));
+  printf("\n");
+  
+  printf("index (3): ");
+  printf("%Iu", ll_get(list, 3));
+  printf("\n");
+  
+  printf("index (4): ");
+  printf("%Iu", ll_get(list, 4));
+  printf("\n");
+  
+  //Out of bounds
+  printf("index (5): ");
+  printf("%Iu", ll_get(list, 5));
+  printf("\n");  
+  
+  //ll_set test
+  printf("preSet: ");
+  printf("%Iu \n", ll_get(list, 0));
+  ll_set(list, 0, 50);
+  printf("postSet: ");
+  printf("%Iu \n", ll_get(list, 0));
+  
+  
+  //ll_remove test
+  print_list(list);
+  ll_remove(list, 1);
+  print_list(list);
+  
+  //ll_insert test
+  ll_insert(list, 4, 900);
+  print_list(list);
+  
   ll_delete(list);
   return 0;
 }
